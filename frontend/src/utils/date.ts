4573ea07d9daaf2c5f7aa12da5aa40d108c51d36
nathan <akkerise@gmail.com>
2025-08-28 13:44:00 +0700

    remove

--- frontend/src/utils/date.ts
+++
@@ -1,3 +0,0 @@
-import dayjs from 'dayjs';
-
-export const formatDate = (d: Date) => dayjs(d).format('YYYY-MM-DD');
--- internal/cache/redis.go
+++
@@ -1,8 +0,0 @@
-package cache
-
-import "github.com/redis/go-redis/v9"
-
-func New(addr string) *redis.Client {
-    return redis.NewClient(&redis.Options{Addr: addr})
-}
-
